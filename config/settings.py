7e6069cc875d2a17569af34938376c3342513ba1
OQdevpy <117621709+OQdevpy@users.noreply.github.com>
2023-10-13 17:45:18 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/topics/settings/
 
 For the full list of settings and their values, see
+
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
